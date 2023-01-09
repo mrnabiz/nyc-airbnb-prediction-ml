897a94319525c6968070276c73d60d2d35cf5d2e
Mohammad Reza Nabizadeh <mrnabiz@student.ubc.ca>
2023-01-09 14:07:08 -0800

    linear model train split fix

--- src/model_building/feat_eng.py
+++ src/model_building/feat_eng.py
@@ -1,14 +1,17 @@
 # author: Mohammad Reza Nabizadeh
 # date: 2023-01-05
 
-"""This script runs the feature engineering and extracts two features from the preprocessed data.
+"""This script runs the feature engineering and extracts two features from 
+the preprocessed data.
 
 
 Usage: feat_eng.py --output_file_path=<output_file_path> --input_file_path=<input_file_path>
  
 Options:
---output_file_path=<output_file_path>   Path to folder and file name where the file with new features will be saved, including the name, `in quotes.
---input_file_path=<input_file_path>		Path to folder and file name where the preprocessed file is stored, including the name, `in quotes.
+--output_file_path=<output_file_path>   Path to folder and file name where the 
+file with new features will be saved, including the name, `in quotes.
+--input_file_path=<input_file_path>		Path to folder and file name where the 
+preprocessed file is stored, including the name, `in quotes.
 """
 
 # Import dependencies
@@ -30,16 +33,23 @@ def main(output_file_path, input_file_path):
     preprocessed_df = pd.read_csv(input_file_path, low_memory=False)
 
     # Apply the feature extractign functions
-    preprocessed_df['host_since'] = pd.to_datetime(preprocessed_df['host_since'])
-    preprocessed_df['last_review'] = pd.to_datetime(preprocessed_df['last_review'])
+    preprocessed_df['host_since'] = pd.to_datetime(
+                                        preprocessed_df['host_since']
+                                        )
+    preprocessed_df['last_review'] = pd.to_datetime(
+                                        preprocessed_df['last_review']
+                                        )
 
     preprocessed_df = preprocessed_df.assign(time_diff=diff_days(
-                                         last_review_date=preprocessed_df['last_review'],
-                                         host_join_date=preprocessed_df['host_since']))
+                            last_review_date=preprocessed_df['last_review'],
+                            host_join_date=preprocessed_df['host_since'])
+                            )
                                          
-    preprocessed_df = preprocessed_df.assign(name_sent=preprocessed_df['name'].apply(get_sentiment))
+    preprocessed_df = preprocessed_df.assign(
+        name_sent=preprocessed_df['name'].apply(get_sentiment)
+        )
 
-    # Clean the inital columns
+    # Clean the initial columns
     drop_columns = ['last_review', 'host_since', 'first_review']
     preprocessed_df = preprocessed_df.drop(columns=drop_columns)
 
